058b7faa24aed59736b16c5df3accd94678a11e7
Kamasah Dickson <kamasahelizabeth15@gmail.com>
2022-10-19 22:30:58 +0000

    mobile faq section updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,17 +1,16 @@
 const mobileNav = document.querySelector('.mobile__navigation');
-// =========tabs===============
+const faqs = document.querySelectorAll('.question');
+const faqImg = document.querySelectorAll('.faq-img');
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.tab-container');
 
 // ==remove transitions after windows loads
 window.addEventListener('load', () => {
 	document.body.classList.remove('trans');
+	optimize();
 });
 
-//================ after-document-loads===========
-window.addEventListener('DOMContentLoaded', () => {
-	document.body.classList.remove('.trans');
-
+function optimize() {
 	const menu = document
 		.getElementById('hamburger')
 		.addEventListener('click', () => {
@@ -42,4 +41,20 @@ window.addEventListener('DOMContentLoaded', () => {
 			contents[index].classList.add('active');
 		});
 	});
-});
+
+	// ========faq-section===============
+		faqs.forEach((faq, index) => {
+			faq.addEventListener('click', () => {
+				faq.classList.toggle('height');
+				faqImg.forEach((img) => {
+					if (!faq.classList.contains('height')) {
+						img.src = '../../images/icon-arrow.svg';
+						img.style.transform = 'rotate(0deg)';
+					} else {
+						faqImg[index].src = '../../images/icon-arrow2.svg';
+						faqImg[index].style.transform = 'rotate(180deg)';
+					}
+				});
+			});
+		});
+}
