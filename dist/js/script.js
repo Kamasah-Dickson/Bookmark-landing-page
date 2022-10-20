7b5e7dd4059a914109127f30a236e0d731cd0c60
Kamasah Dickson <kamasahelizabeth15@gmail.com>
2022-10-20 11:57:09 +0000

    contact section and email validation updated

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,8 +3,11 @@ const faqs = document.querySelectorAll('.question');
 const faqImg = document.querySelectorAll('.faq-img');
 const tabs = document.querySelectorAll('.tab');
 const contents = document.querySelectorAll('.tab-container');
+const errorMessage = document.getElementById('message');
+const errorIcon = document.getElementById('error');
+const errorColor = document.querySelector('.input');
 
-// ==remove transitions after windows loads
+// ==remove transitions after windows load
 window.addEventListener('load', () => {
 	document.body.classList.remove('trans');
 	optimize();
@@ -57,4 +60,40 @@ function optimize() {
 			});
 		});
 	});
+
+	// =======Email-validation=========
+	document.getElementById('emailBtn').addEventListener('click', (e) => {
+		validate();
+	});
+
+	function validate() {
+		const email = document.getElementById('email').value;
+		let regx = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
+		if (regx.test(email)) {
+			removeError();
+		} else if (email === '') {
+			removeError();
+		} else {
+			popError();
+		}
+
+		document.getElementById('email').addEventListener('keydown', (e) => {
+			if (!e.value === regx.test(email)) {
+				popError();
+			} else {
+				removeError();
+			}
+		});
+	}
+
+	function popError() {
+		errorColor.classList.add('invalid-input');
+		errorMessage.style.display = 'block';
+		errorIcon.style.display = 'block';
+	}
+	function removeError() {
+		errorColor.classList.remove('invalid-input');
+		errorMessage.style.display = 'none';
+		errorIcon.style.display = 'none';
+	}
 }
